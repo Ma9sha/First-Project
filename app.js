17f7d549d81c5410c4204b79a4c3b830a4c5cb3f
Manisha Patel <ma9shap.gmail.com>
2019-06-21 17:10:19 +0100

    fixing the navigation page namaste

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var router = express.Router();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +22,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/namaste', (res, req) => {
+  res.render('namaste')
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
